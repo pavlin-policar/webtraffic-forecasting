275f44ece9edd1e6e54ed403c633869a5a3622eb
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-07-15 12:13:01 +0200

    Implement forward chaining

--- validation.py
+++ validation.py
@@ -1,3 +1,7 @@
+from itertools import chain
+
+import numpy as np
+import pandas as pd
 import numpy as np
 
 
@@ -12,3 +16,28 @@ def smape(truth, predictions):
     result /= truth.shape[0]
 
     return result
+
+
+def validate_on_last_days(n_days):
+    pass
+
+
+def forward_chaining(data, folds):
+    # type: (pd.DataFrame, int) -> Generator[pd.DataFrame]
+    assert data.columns.values[0] == 'Page'
+    splits = np.array_split(data.columns.values[1:], folds + 1)
+
+    for i in range(1, folds):
+        train_indices = list(chain(*splits[:i]))
+        test_indices = splits[i + 1]
+        yield data[train_indices], data[test_indices]
+
+
+def validate_forward_chaining():
+    pass
+
+
+if __name__ == '__main__':
+    train = pd.read_csv('data/train_1.csv')
+    for train, test in forward_chaining(train, 5):
+        print(train.shape, test.shape)
